06d95d9050fad4d2ab0efcb98ea9f9674ee88d0c
Scott Woodend <woodendscott@gmail.com>
2016-10-14 14:57:42 -0400

    implement ajax api logic

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -1,10 +1,47 @@
 'use strict';
 let last;
+let movieUrl = "https://andruxnet-random-famous-quotes.p.mashape.com/?cat=movies";
+let famousUrl = "https://andruxnet-random-famous-quotes.p.mashape.com/?cat=famous";
+
 
 $(document).ready(function(){
   $('body').click(function(){
     setBackground();
   });
+
+  $('#movie-quote').click(function(){
+    $.ajax({
+      url: movieUrl,
+      headers: {
+       "X-Mashape-Key": 'qtmx9JW3eZmshXVDmrUgCxrwMAmnp1JJCGmjsnwdDJBSsgeCbp',
+       "Content-Type": "application/x-www-form-urlencoded",
+       "Accept": "application/json"        
+      },
+      success: function(data){
+        let quoteObj = jQuery.parseJSON(data);
+        $(".quote-area").html(quoteObj.quote);
+        $(".author-area").html(quoteObj.author);
+      }
+    });
+  });
+
+    $('#famous-quote').click(function(){
+    $.ajax({
+      url: famousUrl,
+      headers: {
+       "X-Mashape-Key": 'qtmx9JW3eZmshXVDmrUgCxrwMAmnp1JJCGmjsnwdDJBSsgeCbp',
+       "Content-Type": "application/x-www-form-urlencoded",
+       "Accept": "application/json"        
+      },
+      success: function(data){
+        let quoteObj = jQuery.parseJSON(data);
+        $(".quote-area").html(quoteObj.quote);
+        $(".author-area").html(quoteObj.author);
+      }
+    });
+  });
+
+  $()
 });
 
 const backgroundMap = {
@@ -18,7 +55,7 @@ const backgroundMap = {
 function setBackground(){
     let rand = random(last);
     last = rand;
-    $('.flex-container').css('background-color', backgroundMap[rand]);
+    $('body').css('background-color', backgroundMap[rand]);
 }
 
 function random(last){
