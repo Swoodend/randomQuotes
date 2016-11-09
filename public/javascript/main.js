179c442fc687de2b9d61f3aa7eeb1b3a91dc0a3a
Scott Woodend <woodendscott@gmail.com>
2016-11-08 21:48:17 -0500

    organize code better

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -3,8 +3,37 @@ let last;
 let movieUrl = "https://andruxnet-random-famous-quotes.p.mashape.com/?cat=movies";
 let famousUrl = "https://andruxnet-random-famous-quotes.p.mashape.com/?cat=famous";
 
+const backgroundMap = {
+  1:"#FF3B3B",
+  2: "#BF52FF",
+  3: "#78FF52",
+  4: "#FFFE52",
+  5: "#FA892D"
+};
+
+function setBackground(){
+    let rand = random(last);
+    last = rand;
+    $('body').css('background-color', backgroundMap[rand]);
+}
+
+function random(last){
+  let rand = Math.floor(Math.random() * 5) + 1;
+  if(rand === last){
+    if (rand === 1){
+      rand += 1;
+    } else {
+      rand -= 1;
+    }
+  }
+  return rand;
+}
 
 $(document).ready(function(){
+
+  $('.success').fadeOut(3000, function(){
+    $(this).remove();
+  });
   
   $('body').click(function(){
     setBackground();
@@ -49,37 +78,13 @@ $(document).ready(function(){
       method: "POST",
       data: {quoteToTweet: quote},
       success: function(url){
+        hasToken = true;
+        console.log('in success cb hasToken value is', hasToken);
         window.location.replace(url);
       },
       error: function(err){
         console.log(err);
       }
-    });
+    }); 
   });
 });
-
-const backgroundMap = {
-  1:"#FF3B3B",
-  2: "#BF52FF",
-  3: "#78FF52",
-  4: "#FFFE52",
-  5: "#FA892D"
-};
-
-function setBackground(){
-    let rand = random(last);
-    last = rand;
-    $('body').css('background-color', backgroundMap[rand]);
-}
-
-function random(last){
-  var rand = Math.floor(Math.random() * 5) + 1;
-     if(rand === last){
-    if (rand === 1){
-      rand += 1;
-    } else {
-      rand -= 1;
-    }
-  }
-  return rand;
-}
